e968d41273d07608038a3d0e595733ac1e554139
DevMadrine <madrinenamulindwa90@gmail.com>
2025-04-16 11:56:23 +0300

    data types in kotlin

---
+++ src/main/kotlin/datatypes.kt
@@ -0,0 +1,15 @@
+package org.example
+
+fun main(){
+    val number = 5
+    val doubleNumber = 4.99
+    val floatNumber = 4.99f
+    val letter = 'A'
+    val boolean= true
+    val text = "5"
+    val numberFormString =Integer.parseInt(text)
+
+    println(numberFormString)
+    println(number + numberFormString)
+    println(text)
+}
